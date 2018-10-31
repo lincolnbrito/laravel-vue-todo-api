b18f5d41e6b912aec11418140171d1182135c42f
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-31 04:15:04 -0300

    register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class AuthController extends Controller
 {
@@ -36,8 +38,18 @@ class AuthController extends Controller
         }
     }
 
-    public function register(Request $request) 
+    public function register(Request $request)
     {
-        
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
     }
 }
